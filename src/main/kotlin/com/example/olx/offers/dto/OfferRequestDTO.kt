cd9b832d9314506048c011b5f5f256e09501138f
zgrybus <krzysztofnofz@gmail.com>
2025-11-14 11:12:49 +0100

    Added price to Offer entity and add another validaiton step

--- src/main/kotlin/com/example/olx/offers/dto/OfferRequestDTO.kt
+++ src/main/kotlin/com/example/olx/offers/dto/OfferRequestDTO.kt
@@ -1,10 +1,15 @@
 package com.example.olx.offers.dto
 
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 data class OfferRequestDTO(
     @get:NotBlank(message = "Title is required")
     val title: String?,
     @get:NotBlank(message = "Description is required")
-    val description: String?
+    val description: String?,
+    @get:NotNull(message = "Price is required")
+    @get:Min(value = 0, message = "Price cannot be negative")
+    val price: Int?
 )
