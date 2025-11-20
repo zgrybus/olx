279b31e1e77d3ab8da719fd4c8815b51231a95d5
zgrybus <krzysztofnofz@gmail.com>
2025-11-20 19:04:10 +0100

    Added mobile header

---
+++ themes/olx/src/features/header/mobile-header/mobile-header.tsx
@@ -0,0 +1,36 @@
+import { Link } from '@tanstack/react-router';
+import { CirclePlus, Search } from 'lucide-react';
+import type { ElementType } from 'react';
+
+export function MobileNav() {
+  return (
+    <nav className="fixed bottom-0 left-0 z-50 w-full border-t border-border bg-background p-2">
+      <div className="flex items-center justify-center gap-2">
+        <Link to="/" className="group block flex-1">
+          <MobileMenuNavItem icon={Search} label="Search" />
+        </Link>
+        <Link to="/offers" className="group block flex-1">
+          <MobileMenuNavItem icon={CirclePlus} label="Add offer" />
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+type MobileMenuNavItemProps = {
+  icon: ElementType;
+  label: string;
+};
+
+function MobileMenuNavItem({ icon: Icon, label }: MobileMenuNavItemProps) {
+  return (
+    <div
+      className={
+        'flex flex-col gap-1 justify-center items-center transition-colors text-muted-foreground group-[.active]:text-primary'
+      }
+    >
+      <Icon className="size-6" />
+      <span className="text-sm font-medium leading-none">{label}</span>
+    </div>
+  );
+}
--- themes/olx/src/routes/__root.tsx
+++ themes/olx/src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 
 import type { QueryClient } from '@tanstack/react-query';
+import { MobileNav } from '@/features/header/mobile-header/mobile-header';
 
 type MyRouterContext = {
   queryClient: QueryClient;
@@ -10,6 +11,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <div className="bg-slate-100 min-h-dvh w-full bg-slate-10 font-sans text-neutral-900">
       <Outlet />
+      <MobileNav />
     </div>
   ),
 });
