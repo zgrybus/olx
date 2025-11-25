da180d41b029d11c61c62d5ddea957aa4263bf6b
zgrybus <krzysztofnofz@gmail.com>
2025-11-25 14:52:13 +0100

    Added id to UserDetails

--- src/main/kotlin/com/example/olx/auth/service/CustomAuthUserDetailsService.kt
+++ src/main/kotlin/com/example/olx/auth/service/CustomAuthUserDetailsService.kt
@@ -1,25 +1,25 @@
 package com.example.olx.auth.service
 
+import com.example.olx.auth.dto.CustomAuthUserDetailsDTO
 import com.example.olx.users.repository.UserRepository
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
-class CustomUserDetailsAuthService(
+class CustomAuthUserDetailsService(
     val userRepository: UserRepository,
 ) : UserDetailsService {
-    override fun loadUserByUsername(username: String): UserDetails {
+    override fun loadUserByUsername(username: String): CustomAuthUserDetailsDTO {
         val user =
             userRepository.findByUsername(username)
                 ?: throw UsernameNotFoundException("User $username not found")
 
-        return User
-            .builder()
-            .username(user.username)
-            .password(user.password)
-            .build()
+        return CustomAuthUserDetailsDTO(
+            id = user.id!!,
+            username = user.username,
+            password = user.password,
+            authorities = listOf(),
+        )
     }
 }
